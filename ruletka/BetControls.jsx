aedf86ed88fcb29465b8c4021b0cfff31c8b353f
Qwinnner <152279903+Qwinnner@users.noreply.github.com>
2026-03-15 20:20:37 +0100

    Update BetControls.jsx

--- ruletka/BetControls.jsx
+++ ruletka/BetControls.jsx
@@ -1,1 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import './BetControls.css';
+
+const BetControls = ({ placeBet, selectedBetValue, updateBalance }) => {
+  const [betAmount, setBetAmount] = useState(0);
+  const [betType, setBetType] = useState('number');
+  const [betValue, setBetValue] = useState('');
+
+  useEffect(() => {
+    setBetValue(selectedBetValue);
+  }, [selectedBetValue]);
+
+  const handleBetAmountChange = (e) => {
+    setBetAmount(e.target.value);
+  };
+
+  const handleBetTypeChange = (e) => {
+    setBetType(e.target.value);
+    setBetValue('');
+  };
+
+  const handleBetValueChange = (e) => {
+    setBetValue(e.target.value);
+  };
+
+  const handleBetSubmit = () => {
+    placeBet(Number(betAmount), betType, betValue);
+    updateBalance(-Number(betAmount));
+  };
 
